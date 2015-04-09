2a2c802c19cd96a0ed274a257e426040efe37d8c
iabosi <russ.sherman@gmail.com>
2015-04-08 21:31:45 -0700

    Created config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+
+# ============
+# BOT SETTINGS
+# ============
+
+NICK = ""
+IDENT = ""
+REALNAME = ""
+TRIGGER = ""
+HOST = ""
+PORT = 
+PASSWORD = ""
+MASTER = ""
+CHANNEL = ""
+USERHOST = ""
+FIGLETFONT = ""
+BAPI_USER = ""
+BAPI_KEY = ""
